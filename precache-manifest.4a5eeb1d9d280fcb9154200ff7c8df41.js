f47e48d119fb33482f1456cfc1252c9b8e52a025
chivylif <chivy4gud@gmail.com>
2022-08-28 16:39:02 +0100

    Updates

---
+++ precache-manifest.4a5eeb1d9d280fcb9154200ff7c8df41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6691bb90def26caa06d7",
+    "url": "/Findworker-updated-react-app/static/css/main.72321c2d.chunk.css"
+  },
+  {
+    "revision": "6691bb90def26caa06d7",
+    "url": "/Findworker-updated-react-app/static/js/main.6691bb90.chunk.js"
+  },
+  {
+    "revision": "128fd6128b9459c5ab86",
+    "url": "/Findworker-updated-react-app/static/js/1.128fd612.chunk.js"
+  },
+  {
+    "revision": "fe432d602ebb817a869c",
+    "url": "/Findworker-updated-react-app/static/js/2.fe432d60.chunk.js"
+  },
+  {
+    "revision": "824968a642c851bb7bf8",
+    "url": "/Findworker-updated-react-app/static/js/runtime~main.824968a6.js"
+  },
+  {
+    "revision": "b9d5134b3fb5ca570552c03d19539a39",
+    "url": "/Findworker-updated-react-app/static/media/book.b9d5134b.avif"
+  },
+  {
+    "revision": "246f1336fab1c62519e2175f509b187a",
+    "url": "/Findworker-updated-react-app/index.html"
+  }
+];
